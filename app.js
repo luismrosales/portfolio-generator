5e12177d81d271f970e234afb291cae15a8a8b1d
Luis Rosales <luismrosales2@gmail.com>
2021-09-25 17:57:48 -0400

    separated js files to so code looks better

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const generatePage = require("./src/page-template");
+const fs = require("fs");
+const profileDataArgs = process.argv.slice(2);
+const [nameUser, github] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-  // this
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-  console.log("===========");
-  // is the same as this
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+fs.writeFile("index.html", generatePage(nameUser, github), (err) => {
+  if (err) throw new Error(err);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
---
+++ src/page-template.js
@@ -0,0 +1,19 @@
+const generatePage = (nameUser, github) => {
+  return `
+    <!DOCTYPE html>
+    <html lang = 'en'>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA=Compatible" content="ie-edge">
+    </head>
+  
+    <body>
+    <h1>${nameUser}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+   `;
+};
+
+module.exports = generatePage;
