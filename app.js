e2c81dd871eb13091eced646a5b386a5134a65b5
Luis Rosales <luismrosales2@gmail.com>
2021-09-27 20:01:18 -0400

    npm installed

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const generatePage = require("./src/page-template");
-const fs = require("fs");
-const profileDataArgs = process.argv.slice(2);
-const [nameUser, github] = profileDataArgs;
+const inquirer = require("inquirer");
+const { type } = require("os");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "what is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile("index.html", generatePage(nameUser, github), (err) => {
-  if (err) throw new Error(err);
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
